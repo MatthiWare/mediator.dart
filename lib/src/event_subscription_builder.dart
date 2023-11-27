ae073ebb38369fc8d90b6f067c36d6c600f83533
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2023-11-27 08:10:25 +0100

    Remove skip

--- lib/src/event_subscription_builder.dart
+++ lib/src/event_subscription_builder.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:dart_event_manager/event_manager.dart';
 
 part 'event_subscription_builder/map_builder.dart';
-part 'event_subscription_builder/skip_builder.dart';
 part 'event_subscription_builder/where_builder.dart';
 
 /// Builder that is able to subscribe to the [EventManager].
@@ -38,14 +37,6 @@ abstract class EventSubscriptionBuilder<T> {
   /// that pass the [test] clause will be kept for the [EventHandler].
   EventSubscriptionBuilder<T> where(bool Function(T event) test) {
     return _WhereEventSubscriptionBuilder(parent: this, test: test);
-  }
-
-  /// Filters the events
-  ///
-  /// It extends the current builder so that only inputs
-  /// that pass the [test] clause will be kept for the [EventHandler].
-  EventSubscriptionBuilder<T> skip(int count) {
-    return _SkipEventSubscriptionBuilder(parent: this, skips: count);
   }
 
   /// Subscribes to the given [handler].
