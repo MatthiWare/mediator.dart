8044d313cd3045904007494362c95cf2243ac048
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-05-10 08:11:27 +0200

    Add `EventHandler.factory`

--- lib/src/event/handler/event_handler.dart
+++ lib/src/event/handler/event_handler.dart
@@ -5,10 +5,20 @@ typedef EventHandlerFactory<TEvent> = EventHandler<TEvent> Function();
 
 /// Handler for [TEvent].
 abstract interface class EventHandler<TEvent> {
-  /// Function based event handler
+  /// Function based [EventHandler].
+  ///
+  /// Each event the underlying [handler] will be executed.
   const factory EventHandler.function(
     FutureOr<void> Function(TEvent event) handler,
   ) = _FunctionEventHandler;
+
+  /// Factory based [EventHandler]
+  ///
+  /// Each event the underlying [factory] will be instantiated and used
+  /// to handle the [TEvent].
+  const factory EventHandler.factory(
+    EventHandlerFactory<TEvent> factory,
+  ) = _FactoryEventHandler;
 
   /// Handles the given [event].
   FutureOr<void> handle(TEvent event);
@@ -21,4 +31,16 @@ class _FunctionEventHandler<T> implements EventHandler<T> {
 
   @override
   FutureOr<void> handle(T event) => handler(event);
+}
+
+class _FactoryEventHandler<T> implements EventHandler<T> {
+  final EventHandlerFactory<T> factory;
+
+  const _FactoryEventHandler(this.factory);
+
+  @override
+  FutureOr<void> handle(T event) {
+    final handler = factory();
+    return handler.handle(event);
+  }
 }
