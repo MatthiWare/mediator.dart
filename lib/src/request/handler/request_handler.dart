83876a128969d741ff3344de6f2f2b6f35ce2f6b
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-05-10 15:32:35 +0200

    Add factory based request handler

--- lib/src/request/handler/request_handler.dart
+++ lib/src/request/handler/request_handler.dart
@@ -11,10 +11,15 @@ typedef RequestHandlerFactory<TResponse, TRequest extends Request<TResponse>>
 @internal
 abstract interface class RequestHandler<TResponse,
     TRequest extends Request<TResponse>> {
-  /// Function based request handler
+  /// Function based [RequestHandler].
   const factory RequestHandler.function(
     FutureOr<TResponse> Function(TRequest) handler,
   ) = _FunctionRequestHandler;
+
+  /// Factory based [RequestHandler].
+  const factory RequestHandler.factory(
+    RequestHandlerFactory<TResponse, TRequest> factory,
+  ) = _FactoryRequestHandler;
 
   /// Handles the given [request].
   FutureOr<TResponse> handle(TRequest request);
@@ -29,6 +34,7 @@ abstract interface class QueryHandler<TResponse extends Object,
         TQuery extends Query<TResponse>>
     implements RequestHandler<TResponse, TQuery> {}
 
+@immutable
 class _FunctionRequestHandler<TResponse, TRequest extends Request<TResponse>>
     implements RequestHandler<TResponse, TRequest> {
   final FutureOr<TResponse> Function(TRequest) handler;
@@ -37,4 +43,40 @@ class _FunctionRequestHandler<TResponse, TRequest extends Request<TResponse>>
 
   @override
   FutureOr<TResponse> handle(TRequest request) => handler(request);
+
+  @override
+  int get hashCode => Object.hash(runtimeType, handler);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _FunctionRequestHandler<TResponse, TRequest> &&
+            other.handler == handler);
+  }
+}
+
+@immutable
+class _FactoryRequestHandler<TResponse, TRequest extends Request<TResponse>>
+    implements RequestHandler<TResponse, TRequest> {
+  final RequestHandlerFactory<TResponse, TRequest> factory;
+
+  const _FactoryRequestHandler(this.factory);
+
+  @override
+  FutureOr<TResponse> handle(TRequest request) {
+    final handler = factory();
+    return handler.handle(request);
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, factory);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _FactoryRequestHandler<TResponse, TRequest> &&
+            other.factory == factory);
+  }
 }
