17fbbd4cc53817a0aa7addd14cadc238a96e4a5f
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-07-14 16:40:17 +0200

    General purpose `combineLatest`

--- lib/src/event/reactive/combine_latest.dart
+++ lib/src/event/reactive/combine_latest.dart
@@ -30,41 +30,77 @@ void _assertEventManagersTheSame(List<EventSubscriptionBuilder> builders) {
   }
 }
 
+EventSubscriptionBuilder<R> combineLatest<R>(
+  List<EventSubscriptionBuilder<dynamic>> events,
+  R Function(List<dynamic> events) combinator,
+) {
+  assert(() {
+    _assertEventManagersTheSame(events);
+    return true;
+  }());
+
+  final forkedEventManager =
+      (events.first as EventManagerProvider).eventManager.fork();
+
+  final builder = forkedEventManager.on<_WrappedR<R>>();
+
+  final lastValues = List<Object?>.filled(events.length, sentinel);
+  final emittedHandlersList = List<bool>.filled(events.length, false);
+
+  bool allHandlersEmitted = false;
+
+  Future<void> emit() async {
+    if (!allHandlersEmitted) {
+      allHandlersEmitted = !emittedHandlersList.any((x) => x == false);
+
+      if (!allHandlersEmitted) {
+        return;
+      }
+    }
+
+    final result = _WrappedR(combinator(lastValues));
+
+    await forkedEventManager.dispatch<_WrappedR<R>>(result);
+  }
+
+  final subscriptions = events.indexed.map((e) {
+    final index = e.$1;
+    final eventBuilder = e.$2;
+
+    bool firstEvent = true;
+
+    final internalSubscription =
+        eventBuilder.map((e) => e as dynamic).subscribeFunction((event) async {
+      if (firstEvent) {
+        firstEvent = false;
+        emittedHandlersList[index] = true;
+      }
+
+      lastValues[index] = event;
+
+      await emit();
+    });
+
+    return internalSubscription;
+  }).toList(growable: false);
+
+  return builder.map((wrapped) => wrapped.r);
+}
+
 EventSubscriptionBuilder<R> combineLatest2<R, A, B>(
   EventSubscriptionBuilder<A> eventA,
   EventSubscriptionBuilder<B> eventB,
   R Function(A a, B b) combinator,
 ) {
-  final eventManager = EventManager.create();
+  return combineLatest(
+    [eventA, eventB],
+    (values) {
+      final a = values[0] as A;
+      final b = values[1] as B;
 
-  Object? a = sentinel;
-  Object? b = sentinel;
-
-  final builder = eventManager.on<_WrappedR<R>>();
-
-  Future<void> emit() async {
-    if (identical(a, sentinel) || identical(b, sentinel)) {
-      return;
-    }
-
-    final result = _WrappedR<R>(combinator(a as A, b as B));
-
-    eventManager.dispatch(result);
-  }
-
-  final subA = eventA.subscribeFunction((event) async {
-    a = event;
-
-    await emit();
-  });
-
-  final subB = eventB.subscribeFunction((event) async {
-    b = event;
-
-    await emit();
-  });
-
-  return builder.map((wrapped) => wrapped.r);
+      return combinator(a, b);
+    },
+  );
 }
 
 class _WrappedR<R> implements DomainEvent {
