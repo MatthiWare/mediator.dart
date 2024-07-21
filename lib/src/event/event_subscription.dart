f0a4d54f368920900a8c413d7bd0c3fc85c4bb32
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-07-21 10:12:56 +0200

    Add more cancellation functionality (#19)
    * Add `isCanceled`
    
    * Add `doOnCancel`

--- lib/src/event/event_subscription.dart
+++ lib/src/event/event_subscription.dart
@@ -1,10 +1,45 @@
 import 'package:dart_mediator/src/event/handler/event_handler.dart';
 
+/// Canceled callback function.
+typedef CancelCallback = void Function();
+
 /// Can be used to [cancel] the given subscription of a [EventHandler].
 class EventSubscription {
-  final void Function() _unsubscribe;
+  bool _isCanceled = false;
 
-  const EventSubscription(this._unsubscribe);
+  final CancelCallback _unsubscribe;
 
-  void cancel() => _unsubscribe();
+  final _onCancelCallbacks = List<CancelCallback>.empty(growable: true);
+
+  /// Whether the current subscription has been canceled.
+  bool get isCanceled => _isCanceled;
+
+  /// Creates a new [EventSubscription] that will trigger the [_unsubscribe]
+  /// on [cancel].
+  EventSubscription(this._unsubscribe);
+
+  /// Cancels the current subscription.
+  void cancel() {
+    assert(
+      !isCanceled,
+      'Cancel can only be called once.',
+    );
+
+    _unsubscribe();
+    _isCanceled = true;
+
+    for (final callback in _onCancelCallbacks) {
+      callback();
+    }
+  }
+
+  /// Adds a callback [callback] that will be invoked when [cancel] is called.
+  void doOnCancel(CancelCallback callback) {
+    assert(
+      !isCanceled,
+      'This subscription was already canceled.',
+    );
+
+    _onCancelCallbacks.add(callback);
+  }
 }
