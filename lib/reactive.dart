6b24cd912e43098f3fe839cdef719b2be75a656a
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-08-03 07:49:58 +0200

    add library for reactive extensions

---
+++ lib/reactive.dart
@@ -0,0 +1,6 @@
+/// Reactive extensions for events
+library reactive;
+
+export 'src/event/reactive/combine_latest.dart';
+export 'src/event/reactive/merge.dart';
+export 'src/event/reactive/zip.dart';
